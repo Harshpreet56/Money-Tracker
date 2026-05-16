9e1f3100cc68568f87140778a29587ae3d2114dc
Harshpreet56 <dhanoaharshpreet70@gmail.com>
2026-05-16 13:57:35 +0530

    modification

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,20 +1,50 @@
+// import jwt from "jsonwebtoken";
+
+// const protect = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization;
+
+//     if (!token) {
+//       return res.status(401).json({ message: "Not authorized" });
+//     }
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     req.user = decoded;
+
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: "Token failed" });
+//   }
+// };
+
+// export default protect;
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 const protect = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
+  let token;
 
-    if (!token) {
-      return res.status(401).json({ message: "Not authorized" });
+  if (req.headers.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401).json({
+        message: "Not authorized",
+      });
     }
+  }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = decoded;
-
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Token failed" });
+  if (!token) {
+    res.status(401).json({
+      message: "No token",
+    });
   }
 };
 
